069ca76646f37720f3168e6f0e1fd9e9a0140006
Frederick <frederick.schubert@t-online.de>
2026-06-14 23:06:35 +0200

    removed black, white and threshold from dithering shader

--- Scripts/Render/PostProcessRenderer.cs
+++ Scripts/Render/PostProcessRenderer.cs
@@ -15,9 +15,6 @@ namespace Render
         private bool _dithering = true;
         private int _ditheringSize = 10;
         private int _resolutionScale = 2;
-        private float _threshold = 60.0f;
-        private Color _white = Colors.White;
-        private Color _black = Colors.Black;
         private int _quantizeLevels = 4;
 
         // --- Properties mit direktem Shader-Update ---
@@ -52,39 +49,6 @@ namespace Render
             {
                 _resolutionScale = Mathf.Max(1, value); // Mindestens 1
                 _material?.SetShaderParameter("resolution_scale", _resolutionScale);
-            }
-        }
-
-        [Export(PropertyHint.Range, "0,255")]
-        public float Threshold
-        {
-            get => _threshold;
-            set
-            {
-                _threshold = Mathf.Clamp(value, 0f, 255f);
-                _material?.SetShaderParameter("threshold", _threshold);
-            }
-        }
-
-        [Export]
-        public Color White
-        {
-            get => _white;
-            set
-            {
-                _white = value;
-                _material?.SetShaderParameter("white", value);
-            }
-        }
-
-        [Export]
-        public Color Black
-        {
-            get => _black;
-            set
-            {
-                _black = value;
-                _material?.SetShaderParameter("black", value);
             }
         }
 
@@ -123,9 +87,6 @@ namespace Render
             _material.SetShaderParameter("dithering", _dithering);
             _material.SetShaderParameter("dithering_size", _ditheringSize);
             _material.SetShaderParameter("resolution_scale", _resolutionScale);
-            _material.SetShaderParameter("threshold", _threshold);
-            _material.SetShaderParameter("white", _white);
-            _material.SetShaderParameter("black", _black);
             _material.SetShaderParameter("quantize_levels", _quantizeLevels);
         }
     }
